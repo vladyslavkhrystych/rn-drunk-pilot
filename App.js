6cda3a1dc92c6e8d869d873a4799e9eeafe3aa95
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2020-05-21 11:45:14 +0300

    setup world with game objects: wall, ceiling, bird; add physics rules for them; add handler for changing bird position on touch

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
+
+import Bird from "./src/Bird";
+import Wall from "./src/Wall";
+import Physics from "./src/Physics";
+
+import { maxWidth, maxHeight } from "./src/Common/constants";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(maxWidth / 4, maxHeight / 2, 50, 50);
+
+    let floor = Matter.Bodies.rectangle(
+      maxWidth / 2,
+      maxHeight - 25,
+      maxWidth,
+      50,
+      { isStatic: true }
+    );
+
+    let ceiling = Matter.Bodies.rectangle(maxWidth / 2, 25, maxWidth, 50, {
+      isStatic: true,
+    });
+
+    Matter.World.add(world, [bird, floor, ceiling]);
+
+    return {
+      physics: { engine, world },
+      bird: {
+        body: bird,
+        color: "red",
+        size: [50, 50],
+        renderer: Bird,
+      },
+      floor: {
+        body: floor,
+        color: "green",
+        size: [maxWidth, 50],
+        renderer: Wall,
+      },
+      ceiling: {
+        body: ceiling,
+        color: "blue",
+        size: [maxWidth, 50],
+        renderer: Wall,
+      },
+    };
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GameEngine
+          ref={(ref) => {
+            this.gameEngine = ref;
+          }}
+          style={styles.game}
+          systems={[Physics]}
+          entities={this.entities}
+        >
+          <StatusBar hidden={true} />
+        </GameEngine>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
   },
 });
---
+++ src/Common/constants.js
@@ -0,0 +1,5 @@
+import { Dimensions } from "react-native";
+
+export const maxWidth = Dimensions.get("screen").width;
+export const maxHeight = Dimensions.get("screen").height;
+export const gapSize = 200;
